511a26303c5ae44aa3c6ed43917374b36ddc57a2
Seka Shchek <DoomkaSU@gmail.com>
2015-03-23 18:16:26 +0200

    check fields

--- js/login.js
+++ js/login.js
@@ -14,8 +14,19 @@ define (function (require){
     var sendRegistrationData = function () {
 
         var email = $("[name = email]").val(),
-            password = $("[name = password]").val();
+            password = $("[name = password]").val(),
+            error = $("[name=divForError]");
 
+        if(email === '') {
+            error.addClass("divForError").text('Fill in the fields!!!');
+        }else {
+            error.removeClass("divForError");
+        }
+        if(password === '') {
+            error.addClass("divForError").text('Fill in the fields!!!');
+        }else {
+            error.removeClass("divForError").text('');;
+        }
         $.ajax({
             url: "http://freethenumbers.com/auth/user.php?action=registerViaIdentity",
             type:"POST",
@@ -25,13 +36,9 @@ define (function (require){
             },
             dataType: "jsonp"
         }).done(function(){
-
         });
-
-
         console.log("click Login")
     };
-
     $("[name = buttonSingUp]").click(sendRegistrationData);
 
 
@@ -45,6 +52,41 @@ define (function (require){
     $("[name = buttonLogin]").click(sendLogin);
 
 
+
+
+
+/*
+    function addCo  mment () {
+        var nameVal = $('#name').val(),
+            nE = '#nam',
+            cE = '#com',// = //
+            comVal = $('#aCom').val();
+
+        if(nameVal === '') {
+            $(nE).addClass("error").text('You forgot to enter a name!!!');
+        }else {
+            $(nE).removeClass("error");
+            $(nE).text("The author's name:");
+        }
+        if(comVal === '') {
+            $(cE).addClass("error");
+            $(cE).text('You forgot to enter a comment!!!');
+
+        }else {
+            $(cE).removeClass("error");
+            $(cE).text("The author's comment:");
+
+            $.ajax({
+                url: server + "?action=add&author="
+                + nameVal + "&text=" + comVal + "&v=2",
+                dataType: "jsonp"
+            })
+                .done(addCommentsToBuffer);
+//            .fail(messageError);
+            console.log('add comment');
+        }
+    }
+*/
 });
 
 
