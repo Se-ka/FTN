1e187628d6c6fc1585bcc2007bafd9fc9eaf3963
Dima Gorbenko <dmitriy.gorbenko@gmail.com>
2015-03-30 17:47:29 +0300

    rendering list of tables with underscore

--- js/require-config.js
+++ js/require-config.js
@@ -3,6 +3,7 @@ requirejs.config({
 	paths: {
 		text: "js/lib/require-plugin-text",
 		jquery: "js/lib/jquery-2.1.3.min",
+        underscore: "js/lib/underscore-min",
 		cssLoader: "js/lib/require-plugin-css",
         store: "js/lib/amplify.store"
 	}
